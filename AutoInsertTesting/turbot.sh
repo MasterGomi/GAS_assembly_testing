36cf96a9a3d64550d8205e3c300d0623fd8fcfb7
Jackson <jacksongray36@gmail.com>
2021-08-08 17:03:54 +1000

    Cleaned up turbot.sh

--- AutoInsertTesting/turbot.sh
+++ AutoInsertTesting/turbot.sh
@@ -1,6 +1,6 @@
 # $1 should be target program to disassemble
 
-ddisasm $1 --asm temp.s > /dev/null
+ddisasm $1 --asm temp.s > /dev/null 2>&1
 rm $1
 sed -i -e '/ret/i call HelloWorld' -e '3r HelloWorldInject.s' temp.s
 gcc -o $1 temp.s
